057da85b13338f4d1c112f843e301c5bc408b3b2
Elijah Reyes <55816151+Elijah-Reyes@users.noreply.github.com>
2024-12-16 13:19:59 -0800

    Add files via upload

---
+++ src_for_github/plotting.R
@@ -0,0 +1,18 @@
+## make a colour transparent
+makeTransparent <- function(..., alpha=0.5) {
+  if(alpha<0 | alpha>1) stop("alpha must be between 0 and 1")
+  alpha = floor(255*alpha)  
+  newColor = col2rgb(col=unlist(list(...)), alpha=FALSE)
+  .makeTransparent = function(col, alpha) {
+    rgb(red=col[1], green=col[2], blue=col[3], alpha=alpha, maxColorValue=255)
+  }
+  apply(newColor, 2, .makeTransparent, alpha=alpha)
+}
+
+## nice little pdf function
+pdf.f <- function(f, file, ...) {
+  cat(sprintf('Writing %s\n', file))
+  pdf(file, ...)
+  on.exit(dev.off())
+  f()
+}
