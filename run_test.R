e13273705b2c1f193a327acc4f1fdb2402c716db
Elijah Reyes <55816151+Elijah-Reyes@users.noreply.github.com>
2024-12-16 12:48:11 -0800

    Update run_test.R

--- run_test.R
+++ run_test.R
@@ -10,12 +10,12 @@ source('src/init.R')
 ## Runs model; see prms.R for default parameters; 
 ## any parameters in the prms.R file can be updated here as an arguement to run.model()    
 
-out <- run.model(male.care.inits = c(0.9,0.05),   
-                 female.care.inits = c(0.05,0.9),   
-                 male.sex.inits = c(0.05,0.9),    
-                 female.sex.inits = c(0.9,0.05),
+out <- run.model(male.care.inits = c(0,9),   
+                 female.care.inits = c(9,0),   
+                 male.sex.inits = c(7,1),    
+                 female.sex.inits = c(1,7),
                  n.patches = 2,  
-                 mate.system='limited.sex', ##Choose 1: 'limited.sex' 'limited.choosy', choosy.sex
+                 mate.system='limited.sex', ##Choose 1: 'CTM' 'MPM' 'noSS.CTM'
                  num.gens=1e4,
                  store.fit=TRUE,
                  store.growth=TRUE,
